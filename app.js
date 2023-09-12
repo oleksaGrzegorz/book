6632f565e4c8491110db9c1c8e5b2f11c12b0c00
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-09-12 15:34:22 +0200

    className in js changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let buttonElement = document.querySelector(".button");
 let bodyElement = document.querySelector(".body");
 
-buttonElement.addEventListener("click", () =>
-  bodyElement.classList.toggle("dark")
-);
+buttonElement.addEventListener("click", () => {
+  bodyElement.classList.toggle("darkBackground");
+});
