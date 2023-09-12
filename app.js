1bc18506dc92841f188591f636fe2a3faa401631
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-09-12 09:11:54 +0200

    test-js code deleted

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-console.log("hi Greg");
-let a = 10;
-console.log(a);
-prompt("lets start!");
-console.log(Math.floor(Math.random() * 100));
-let variable1 = 5;
-variable1 = 6;
-console.log(variable1);
-let question = 'Czy chcesz poznać język JavaScript? "Tak!"';
-let intNr = 1;
-let decNr = 1.5;
-let expNr = 1.4e15;
-let octNr = 0o10; 
 
-console.log(intNr,decNr,expNr,octNr);
