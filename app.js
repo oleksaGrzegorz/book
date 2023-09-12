93035cb2aa91a088d77a95c9e2f66f2c7772e9f1
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-09-12 09:44:32 +0200

    changeBackgroundColor button added

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+let buttonElement = document.querySelector(".button");
+let bodyElement = document.querySelector(".body");
 
+buttonElement.addEventListener("click", () =>
+  bodyElement.classList.toggle("dark")
+);
