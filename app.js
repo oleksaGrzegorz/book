219e965c99ae16b40914a6c9aee4d1eb7b187ec8
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-09-13 18:23:19 +0200

    button for changing theme added

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ let bodyElement = document.querySelector(".body");
 
 buttonElement.addEventListener("click", () => {
   bodyElement.classList.toggle("darkBackground");
+  bodyElement.classList.contains("darkBackground")
+    ? (buttonElement.innerText = "Set light theme")
+    : (buttonElement.innerText = "Set dark theme");
 });
